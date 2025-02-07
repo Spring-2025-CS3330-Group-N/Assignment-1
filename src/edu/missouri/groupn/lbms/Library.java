d65447e8752ef5cb5049e6c2e76c225a3b69faa8
keemcc <ktsmccann@gmail.com>
2025-02-06 22:02:32 -0600

    Added Javadocs to Library class, for the class, attributes and addBook

--- src/edu/missouri/groupn/lbms/Library.java
+++ src/edu/missouri/groupn/lbms/Library.java
@@ -1,9 +1,29 @@
 package edu.missouri.groupn.lbms;
 
-public class Library {	
+/**
+ * This class stores data relating to a library.
+ * The library will contain an array of Book objects within it
+ * as well as an integer count of the number of books currently in it.
+ */
+public class Library {
+	/**
+	 * Book object array to store books within the library.
+	 * Current maximum is set to 5 books.
+	 */
 	private Book[] books = new Book[5];
+	/**
+	 * Integer count of number of books currently stored in the library.
+	 */
 	private int count = 0;
 	
+	/**
+	 * Book Adder:
+	 * This method will first check if the library it is called upon is full.
+	 * If the library is not full, it will add the passed book to the library and return true.
+	 * If the library is full, it will print an error and return false.
+	 * @param book Book object to be added to library.
+	 * @return Boolean true or false depending on if it is successful (true) or not (false).
+	 */
 	public boolean addBook(Book book) {
 		if (this.count >= 5) {
 			System.err.println("The library is full!");
