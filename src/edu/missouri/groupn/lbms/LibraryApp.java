851a288a78463431c8676dfb1c63222e07abd1fa
Alyssa Norrenberns <alyssa.norrenberns@gmail.com>
2025-02-12 16:56:40 -0600

    created LibraryApp class
    added placeholder comments for the assignment requirements

---
+++ src/edu/missouri/groupn/lbms/LibraryApp.java
@@ -0,0 +1,13 @@
+package edu.missouri.groupn.lbms;
+
+public class LibraryApp {
+
+	public static void main(String[] args) {
+		// create library object
+		// add 3 different books with setters and constructors
+		// search for a book by ISBN and display result
+		// remove a book that exists and does not exist
+		// displays all books in library
+	}
+
+}
